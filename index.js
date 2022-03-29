f436cc939d58e3b5213ef690d149a4da138b8576
Blazkowicz <bonnet.nicolas66@gmail.com>
2022-03-29 12:19:47 +0200

    Nettoyage du code

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-let Parser = require('rss-parser');
-
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
@@ -16,19 +14,3 @@ require('./app/routes/feed.route')(app);
 app.listen(3000, function(){ 
     console.log("server started")
 });
-/* let parser = new Parser();
-let url = ["https://www.journaldugeek.com/feed/", "https://gamergen.com/rss"];
-
-(async () => {
-    url.forEach(u => {
-        toto(u);
-    })
-})();
-
-async function toto(u) {
-    let feed = await parser.parseURL(u);
-    console.log(feed.title);
-    feed.items.forEach(item => {
-        console.log(item)
-    });
-} */
